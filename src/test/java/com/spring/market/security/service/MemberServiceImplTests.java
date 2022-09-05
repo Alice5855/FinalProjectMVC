a49ea6532a880926e14f4135a83224513d43d252
Parkryoug <105841250+Parkryoug@users.noreply.github.com>
2022-09-05 11:42:21 +0900

    member 기능 구현 회원가입만됨
    member기능구현

---
+++ src/test/java/com/spring/market/security/service/MemberServiceImplTests.java
@@ -0,0 +1,81 @@
+package com.spring.market.security.service;
+
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.market.security.model.Member;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/security-context.xml"
+})
+@Log4j
+public class MemberServiceImplTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private DataSource ds;
+	
+	@Setter(onMethod_ = @Autowired)
+	private MemberService ms;
+	
+	
+	
+//	@Test
+//	public void testsignup() {
+//		System.out.println("회원가입시작");
+//		Member mem = new Member();
+//		System.out.println("Memeber 맴버 담겨짐?" + mem);
+//		mem.setMemName("김춘식");
+//		mem.setMemEmail("kimchunsic@naver.com");
+//		mem.setMemNickname("kimchunsic");
+//		mem.setMemPw("kimchunsic");
+//		mem.setMemAd("어쩌라구");
+//		mem.setMemPh("010-2661-9397");
+//		mem.setMemPimg("프로필사진담는다");
+//		mem.setMemGender("전투헬리콥터!!!");
+//		
+//		System.out.println("이름가져와짐? ?" + mem.getMemName());
+//	 	ms.signup(mem);
+//		System.out.println("mem 회원가입이됨?" + mem);
+//	}
+//
+//	@Test
+//	public void testUpdate() {
+//		Member mem = ms.MemInfo("kimchunsic");
+//		System.out.println("mem에 뭐담김?" + mem);
+//		if(mem == null) {
+//			System.out.println("널임?");
+//			return;
+//		}else {
+//			mem.setMemAd("주소바꿉니다");
+//		}
+//		System.out.println("수덩결과좀 보여주세요" + ms.update(mem));
+//	}
+//
+	@Test
+	public void testDeleteMember() {
+		try {
+			ms.deleteMember(4L);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		} 
+	}
+//
+//	@Test
+//	public void testMemInfo() {
+//		fail("Not yet implemented");
+//	}
+
+}
