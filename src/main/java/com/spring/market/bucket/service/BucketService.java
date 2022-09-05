f0d80ca752dad0e2e19ce78ae678b4abfc42afb7
thdtl5487 <thdtl5487@gmail.com>
2022-09-05 12:10:59 +0900

    PurchaseVO 생성, BucketService 작성시작

---
+++ src/main/java/com/spring/market/bucket/service/BucketService.java
@@ -0,0 +1,11 @@
+package com.spring.market.bucket.service;
+
+import com.spring.market.product.domain.ProductVO;
+
+public interface BucketService {
+	
+	public void register(ProductVO product);
+	
+	
+	
+}
