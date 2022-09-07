5108998a7090de2986d0b78d947a2d6dadfa7e99
thdtl5487 <thdtl5487@gmail.com>
2022-09-07 09:51:43 +0900

    결제 기능 관련 패키지 추가
    결제 관련 기능 정의
    현재 테스트에서 PdNum을 null로 넣지 못하는 이슈 있음

---
+++ src/main/java/com/spring/market/purchase/domain/PurchaseVO.java
@@ -0,0 +1,23 @@
+package com.spring.market.purchase.domain;
+
+import lombok.Data;
+
+@Data
+public class PurchaseVO {
+	
+	private Long memNum;
+	private Long pcNum;
+	private String pcAddress;
+	private String pcTel;
+	private Long pdNum1;
+	private Long pdNum2;
+	private Long pdNum3;
+	private Long pdNum4;
+	private Long pdNum5;
+	private Long pdNum6;
+	private Long pdNum7;
+	private Long pdNum8;
+	private Long pdNum9;
+	private Long pdNum10;
+	
+}
---
+++ src/main/java/com/spring/market/purchase/mapper/PurchaseMapper.java
@@ -0,0 +1,9 @@
+package com.spring.market.purchase.mapper;
+
+import com.spring.market.purchase.domain.PurchaseVO;
+
+public interface PurchaseMapper {
+	
+	public void registerPurchase(PurchaseVO purchaseVO);
+
+}
